88e2d54b93f2b4d9ca24feea7d0cfaf52d18a2c3
AshishV306 <110739725+AshishV306@users.noreply.github.com>
2023-11-22 19:11:57 +0530

    created signUp and route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import  express  from "express";
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -17,4 +18,6 @@ app.listen(3000, ()=>{
     console.log("Server is running on port 3000")
 })
 
+app.use(express.json()); // we use this because when we use post request for testing in thunderclient for api/auth/sign we get undefined in console but when we use this we allow to send json to server so its get printed in console
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
