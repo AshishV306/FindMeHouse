7af5b9f821e767ac78ed2cd1979a3e7c367d8186
AshishV306 <110739725+AshishV306@users.noreply.github.com>
2023-11-25 01:34:01 +0530

    Created User Update api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -19,6 +20,8 @@ app.listen(3000, ()=>{
 })
 
 app.use(express.json()); // we use this because when we use post request for testing in thunderclient for api/auth/sign we get undefined in console but when we use this we allow to send json to server so its get printed in console
+app.use(cookieParser());
+
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 
