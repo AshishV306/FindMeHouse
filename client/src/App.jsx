5c977001753cc9939c09af1fe673c666765efa0b
AshishV306 <110739725+AshishV306@users.noreply.github.com>
2023-12-27 13:06:58 +0530

    Completed Upadte Listing functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import About from './pages/About'
 import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing';
+import UpdateListing from './pages/UpdateListing';
 
 export default function App() {
   return <BrowserRouter>
@@ -21,6 +22,10 @@ export default function App() {
     <Route element={<PrivateRoute />}>
       <Route path="/profile" element = {<Profile/>}/>
       <Route path='/create-listing' element={<CreateListing />} />
+      <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          />
     </Route>
     
   </Routes>
